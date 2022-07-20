5902fedb0a1047c6918aeec5ca9b494ebdffd9cc
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-07-20 17:19:17 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# On-Chain-Netflix
-Netflix on chain
+https://netflix-decentralized-clone.vercel.app/
+
+1) Decentralized Netflix Clone  made using Js , Morzlis , React , Web3UIkit
+2) Create the .env file using sample.env
+3) Insert your Moralis appId and ServerUrl key into .env
+4) yarn 
+5) yarn start
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from 'web3uikit';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId={process.env.REACT_APP_appId} serverUrl={process.env.REACT_APP_serverUrl}>
+    <MoralisProvider appId="IECBbRIOTV9bT7M2sbcf0ZE6jdMmuAkOpBufU6aO" serverUrl="https://jal8fchjfvo2.usemoralis.com:2053/server">
+      <NotificationProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      </NotificationProvider>
+    </MoralisProvider>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
